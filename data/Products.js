e58e6f33e0b7842c62aeb7e20f319d8e5582dce6
9inelife <preshomezi@gmail.com>
2022-10-28 08:55:45 +0100

    servershop
    heroku

---
+++ data/Products.js
@@ -0,0 +1,155 @@
+const products = [ 
+  {
+    name: "Spotter Hoodie (Blue)",
+    image: "/images/12.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (Grey)",
+    image: "/images/11.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (Grey) With All Traits",
+    image: "/images/10.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (Black) With All Traits",
+    image: "/images/1.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (Black)",
+    image: "/images/2.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (White)",
+    image: "/images/13.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (White) With All Traits",
+    image: "/images/14.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter Hoodie (Blue) With All Traits",
+    image: "/images/15.jpg",
+    description:
+      "Not Available Yet",
+    price: 599,
+    countInStock:0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Spotter T-shirt(blue)",
+    image: "/images/3.jpg",
+    description:
+      "Spotter Boy",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(White)",
+    image: "/images/4.jpg",
+    description:
+      "Spotter Boy",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(Black)",
+    image: "/images/5.jpg",
+    description:
+      "Spotter Boy",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(Grey)",
+    image: "/images/6.jpg",
+    description:
+      "Spotter with all Cool Characters combination",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(Blue)",
+    image: "/images/7.jpg",
+    description:
+      "Spotter with all Cool Characters combination",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(White)",
+    image: "/images/8.jpg",
+    description:
+      "Spotter with all Cool Characters combination",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Spotter T-shirt(Black)",
+    image: "/images/9.jpg",
+    description:
+      "Spotter with all Cool Characters combination",
+    price: 89,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 4,
+  },
+  
+];
+
+export default products;
